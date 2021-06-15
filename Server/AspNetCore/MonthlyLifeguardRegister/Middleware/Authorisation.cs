d4c4d1b318dab554dd7bb16c834e5368f0dfa201
midnightgizmo <scottamillard@hotmail.com>
2021-06-15 12:17:03 +0100

    Created Authorisation Middleware
    Should have added this a few commits back as the startup.cs was calling on it.
    TODO: Need to implment the login inside the class to carry out the authorisation.

---
+++ Server/AspNetCore/MonthlyLifeguardRegister/Middleware/Authorisation.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonthlyLifeguardRegister.Middleware
+{
+    /// <summary>
+    /// Checks the incoming request to see if they have brought with them a jwt cookie.
+    /// If they have the jwt cookie, check to see to if the jwt cookie is valid. If it is,
+    /// give them acess to the routes that require authorisation.
+    /// </summary>
+    public class AuthorisationMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public AuthorisationMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            //httpContext.Request.Cookies
+            await _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class AuthorisationMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseAuthorisationMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CorsMiddleware>();
+        }
+    }
+}
