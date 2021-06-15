90a627529ec752adcaad79022ddecd6ef83a3145
midnightgizmo <scottamillard@hotmail.com>
2021-06-15 13:44:53 +0100

    create SqLiteConnection.cs class created an SqLite connection class to be able to interact with the sql database

---
+++ Server/AspNetCore/MonthlyLifeguardRegister/Classess/Database/SqLiteConnection.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonthlyLifeguardRegister.Classess.Database
+{
+    public class SqLiteConnection
+    {
+        private SqliteConnection _con;
+        /// <summary>
+        /// Atempts to open a connection to an SQLite database
+        /// </summary>
+        /// <param name="location">the physical path to the SQLite database file</param>
+        /// <returns>true if sucsefull, else false</returns>
+        public bool openConnection(string location)
+        {
+            try
+            {
+                this._con = new SqliteConnection(location);
+                this._con.Open();
+
+                return true;
+
+            }
+            catch (Exception e)
+            {
+
+                return false;
+            }
+
+        }
+
+
+        /// <summary>
+        /// Attemps to close the SQLite database connection 
+        /// </summary>
+        /// <returns>true if sucsefull, else false</returns>
+        public bool CloseConnection()
+        {
+            try
+            {
+                this._con.Close();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+        }
+
+
+
+        public SqliteDataReader ExecuteSelectCommand(string SQLCommand)
+        {
+            SqliteCommand sqliteCommand;
+
+            sqliteCommand = this._con.CreateCommand();
+            sqliteCommand.CommandText = SQLCommand;
+
+            try
+            {
+                return sqliteCommand.ExecuteReader();
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+
+        }
+
+
+        public SqliteDataReader ExecuteParameterizedSelectCommand(string SQLCommand, SqliteParameter[] parameters) //$parameters = SQLiteParameter[]
+        {
+            SqliteCommand sqliteCommand;
+
+            sqliteCommand = this._con.CreateCommand();
+            sqliteCommand.CommandText = SQLCommand;
+
+
+            sqliteCommand.Parameters.AddRange(parameters);
+            
+            try
+            {
+                return sqliteCommand.ExecuteReader();
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        public int ExecuteParameterizedNoneReader(string SQLCommand, SqliteParameter[] parameters)
+        {
+            SqliteCommand sqliteCommand;
+
+            sqliteCommand = this._con.CreateCommand();
+            sqliteCommand.CommandText = SQLCommand;
+
+
+            sqliteCommand.Parameters.AddRange(parameters);
+
+            try
+            {
+                return sqliteCommand.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                return -1;
+            }
+        }
+
+        public int get_last_insert_id()
+        {
+            SqliteCommand sqliteCommand;
+
+            sqliteCommand = this._con.CreateCommand();
+            sqliteCommand.CommandText = "select last_insert_rowid()";
+
+            try
+            {
+                return (int)sqliteCommand.ExecuteScalar();
+            }
+            catch(Exception e)
+            {
+                return -1;
+            }
+        }
+
+    }
+}
