a87822fa5859992acb2c9ee01a1c0e40cf0ddd52
midnightgizmo <scottamillard@hotmail.com>
2021-06-16 10:20:43 +0100

    create env variable sqlConectionStringLocation created env variable called sqlConectionStringLocation which holds the physical location to where the database file exists on the server.

--- Server/AspNetCore/MonthlyLifeguardRegister/Models/AppSettings.cs
+++ Server/AspNetCore/MonthlyLifeguardRegister/Models/AppSettings.cs
@@ -7,10 +7,18 @@ namespace MonthlyLifeguardRegister.Models
 {
     public class AppSettings
     {
+        /// <summary>
+        /// Domain name the client is running on (e.g. localhost)
+        /// </summary>
         public string DomainName { get; set; }
         /// <summary>
         ///  The key used when creating the jwt
         /// </summary>
         public string jwtsecretKey { get; set; }
+
+        /// <summary>
+        /// Physical location on disk to where the database is
+        /// </summary>
+        public string sqlConectionStringLocation { get; set; }
     }
 }
