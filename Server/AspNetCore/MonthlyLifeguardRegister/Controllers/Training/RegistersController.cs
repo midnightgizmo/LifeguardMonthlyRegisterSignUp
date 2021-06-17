4d8e6d737a7a427035241ccc08c544fe4817922a
midnightgizmo <scottamillard@hotmail.com>
2021-06-17 14:02:10 +0100

    Created controller & logic to process the getUpcomingTrainingRegisters.php get request Created RegistersController which handles the /Training/TrainingRegister/getUpcomingTrainingRegisters.php route. Sends back to the user a list of training register between beginning of current month and 6 months after.

---
+++ Server/AspNetCore/MonthlyLifeguardRegister/Controllers/Training/RegistersController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using MonthlyLifeguardRegister.Attributes;
+using MonthlyLifeguardRegister.Classess.Database.Training;
+using MonthlyLifeguardRegister.ControllersLogic.Training;
+using MonthlyLifeguardRegister.Models;
+using MonthlyLifeguardRegister.Models.Training;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonthlyLifeguardRegister.Controllers.Training
+{
+    /// <summary>
+    /// Deals with users logging into the app
+    /// </summary>
+    [ApiController]
+    [Route("API/Training/TrainingRegister")]
+    public class RegistersController : Controller
+    {
+        public RegistersController(IOptions<AppSettings> appSettings)
+        {
+            this.AppSettings = appSettings.Value;
+        }
+
+        /// <summary>
+        /// The settings from the appsettings.json file
+        /// </summary>
+        public AppSettings AppSettings { get; }
+
+
+        [HttpGet]
+        [Authorize]
+        [Route("getUpcomingTrainingRegisters.php")]
+        [Produces("application/json")]
+        public List<TrainingRegister> GetUpcomingRegisters()
+        {
+            GetUpcomingRegistersControllerLogic getUpcomingRegistersControllerLogic = new GetUpcomingRegistersControllerLogic();
+            
+            return getUpcomingRegistersControllerLogic.GetUpcomingRegisters_CreateLoginResponse(this.AppSettings);
+        }
+    }
+}
---
+++ Server/AspNetCore/MonthlyLifeguardRegister/ControllersLogic/Training/GetUpcomingRegistersControllerLogic.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using MonthlyLifeguardRegister.Classess.Database;
+using MonthlyLifeguardRegister.Classess.Database.Training;
+using MonthlyLifeguardRegister.Models;
+using MonthlyLifeguardRegister.Models.Training;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonthlyLifeguardRegister.ControllersLogic.Training
+{
+    public class GetUpcomingRegistersControllerLogic
+    {
+        /// <summary>
+        /// Gets a list of training registers that are avalable between the first of this month and six months from start of this month
+        /// </summary>
+        /// <param name="appSettings">appsettings.json file content</param>
+        /// <returns></returns>
+        public List<TrainingRegister> GetUpcomingRegisters_CreateLoginResponse(AppSettings appSettings)
+        {
+            SqLiteConnection sqlCon;
+            dbSQLiteTrainingRegister dbTrainingRegister;
+            // create a date that will be the first of the current month
+            DateTime dateAtStartOfMonth = this.createDateAtBeginingOfMonth();
+            // create a date six months on from the above date
+            DateTime dateInSixMonthsTime = dateAtStartOfMonth.AddMonths(6);
+            List<TrainingRegister> listOfTrainingRegisters = new List<TrainingRegister>();
+
+            // create a database connection and open it
+            sqlCon = new SqLiteConnection();
+            sqlCon.openConnection(appSettings.sqlConectionStringLocation);
+
+            dbTrainingRegister = new dbSQLiteTrainingRegister(sqlCon);
+            // get all registers between the start of the month and six months on from now
+            listOfTrainingRegisters = dbTrainingRegister.select_dateTimeWhenRegisterIsActive_Between(this.convertToUnixTimeStamp(dateAtStartOfMonth),this.convertToUnixTimeStamp(dateInSixMonthsTime));
+            // close the sqlite connection
+            sqlCon.CloseConnection();
+
+            return listOfTrainingRegisters;
+        }
+
+        /// <summary>
+        /// Creates a DateTime that is set to the first day of current year/month
+        /// </summary>
+        /// <returns>DateTime set to first day of month</returns>
+        private DateTime createDateAtBeginingOfMonth()
+        {
+            DateTime now = DateTime.Now;
+            DateTime startOfMonth = new DateTime(now.Year, now.Month, 1);
+
+            return startOfMonth;
+        }
+
+
+
+        /// <summary>
+        /// Converts the passed in DateTime to a unix time stamp
+        /// </summary>
+        /// <param name="date">Date to be converted</param>
+        /// <returns></returns>
+        private long convertToUnixTimeStamp(DateTime date)
+        {
+            return ((DateTimeOffset)date).ToUnixTimeSeconds();
+        }
+    }
+}
