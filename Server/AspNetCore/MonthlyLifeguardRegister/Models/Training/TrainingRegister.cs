67f8bf1a4f9b8c09162f26b00231ef4fc1808deb
midnightgizmo <scottamillard@hotmail.com>
2021-06-17 13:38:33 +0100

    Created TrainingRegister model class TrainingRegister will be used when interacting with the database.

---
+++ Server/AspNetCore/MonthlyLifeguardRegister/Models/Training/TrainingRegister.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonthlyLifeguardRegister.Models.Training
+{
+    public class TrainingRegister
+    {
+
+        /// <summary>
+        /// Id of the training register in the database
+        /// </summary>
+        public int id { get; set; } = 0;
+
+        /// <summary>
+        /// Idicates when this training should take place.
+        /// Unix Time, the number of seconds since 1970-01-01 00:00:00 UTC.
+        /// </summary>
+        public long dateTimeOfTraining { get; set; } = 0;
+
+        /// <summary>
+        /// Indicates the earilest date/time when this register appears to users.
+        /// Unix Time, the number of seconds since 1970-01-01 00:00:00 UTC.
+        /// </summary>
+        public long dateTimeWhenRegisterIsActive { get; set; } = 0;
+
+        /// <summary>
+        /// Indicatees the earlyist date/time users can start adding there name to this register
+        /// Unix Time, the number of seconds since 1970-01-01 00:00:00 UTC.
+        /// </summary>
+        public long dateTimeFromWhenRegisterCanBeUsed{ get; set; } = 0;
+
+        /// <summary>
+        /// The maxium number of people allowed on this register before it becomes full
+        /// </summary>
+        public int maxNoCandidatesAllowed { get; set; } = 0;
+    }
+}
