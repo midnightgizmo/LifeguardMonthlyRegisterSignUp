90c7a749a7c5681559e61c9dfaabc893312b9d51
midnightgizmo <scottamillard@hotmail.com>
2021-06-29 10:41:44 +0100

    feat(admin): add controler and controler logic for logging admin in user will send admin login credentionals which the server will check.If they are correct, server will create a jwt to send back to the client in a cookie.

---
+++ Server/AspNetCore/MonthlyLifeguardRegister/Controllers/Admin/Authentication/LoginAdminController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using MonthlyLifeguardRegister.ControllersLogic.Admin.Authentication;
+using MonthlyLifeguardRegister.Models;
+using MonthlyLifeguardRegister.Models.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonthlyLifeguardRegister.Controllers.Admin.Authentication
+{
+    [ApiController]
+    [Route("API/admin/Authentication/Login.php")]
+    public class LoginAdminController : ControllerBase
+    {
+        public LoginAdminController(IOptions<AppSettings> appSettings)
+        {
+            this.AppSettings = appSettings.Value;
+        }
+
+        /// <summary>
+        /// The settings from the appsettings.json file
+        /// </summary>
+        public AppSettings AppSettings { get; }
+
+        [HttpPost]
+        [Produces("application/json")]
+        public LoginAuthentication Index([FromForm] string userName, [FromForm] string password)
+        {
+            LoginAdminControllerLogic loginAdminControllerLogic = new LoginAdminControllerLogic();
+
+            // checks if the passed in creditionsal are correct and creates a jwt if they are.
+            // Returns a response to let the user know if they have been logged in.
+            return loginAdminControllerLogic.AuthenticateAdmin_CreateLoginResponse(userName, password, AppSettings, this.HttpContext.Response);
+        }
+    }
+}
---
+++ Server/AspNetCore/MonthlyLifeguardRegister/ControllersLogic/Admin/Authentication/LoginAdminControllerLogic.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Http;
+using MonthlyLifeguardRegister.Classess.Database;
+using MonthlyLifeguardRegister.Classess.Database.Admin;
+using MonthlyLifeguardRegister.Classess.Security;
+using MonthlyLifeguardRegister.Models;
+using MonthlyLifeguardRegister.Models.Authentication;
+using MonthlyLifeguardRegister.Models.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonthlyLifeguardRegister.ControllersLogic.Admin.Authentication
+{
+    public class LoginAdminControllerLogic
+    {
+        public LoginAuthentication AuthenticateAdmin_CreateLoginResponse(string userName, string password, AppSettings appSettings, HttpResponse httpResponse)
+        {
+            LoginAuthentication loginAuthentication;
+            Authorize authorize;
+
+            authorize = new Authorize(appSettings);
+
+            // check the credentials the user sent against the ones in the database
+            if (authorize.AuthorizeAdminUser(userName, password) == false)
+                return this.CreateBadLogin();
+            
+            // create a sucsefull response & create the json web token & create the cookie that
+            // lets the admin user stay logged in
+            loginAuthentication = this.CreateSucsessLoginResponse(appSettings,httpResponse);
+
+            // return the sucsefull response.
+            return loginAuthentication;
+        }
+
+
+
+        /// <summary>
+        /// Creates a sucess response and creates and sets the jwt cookie needed for the admin to be logged in
+        /// </summary>
+        /// <param name="appSettings">needed when creating the json web token and cookie</param>
+        /// <param name="httpResponse">Used to create the cookie</param>
+        /// <returns>response to send back to the client to indicate login was sucsesfull</returns>
+        private LoginAuthentication CreateSucsessLoginResponse(AppSettings appSettings, HttpResponse httpResponse)
+        {
+            LoginAuthentication loginAuthentication;
+
+            loginAuthentication = new LoginAuthentication();
+
+            // set to true to indicate login was sucsefull
+            loginAuthentication._isLoggedIn = true;
+            // string version of the json web stoken
+            loginAuthentication._jwt = this.CreateJwtAndSetCookie(appSettings, httpResponse);
+
+            return loginAuthentication;
+        }
+
+
+        /// <summary>
+        /// Creates a jwt with the users details and creates a cookie which will hold the created jwt
+        /// </summary>
+        /// <param name="userFullDetails">details of the user logging in</param>
+        /// <param name="appSettings">Aappsettings.json file data</param>
+        /// <param name="httpResponse">Allows us to set the cookie</param>
+        /// <returns>the string version of the jwt</returns>
+        private string CreateJwtAndSetCookie(AppSettings appSettings, HttpResponse httpResponse)
+        {
+            // set the cookie to expire in 30 days
+            DateTime cookieExpireDate = DateTime.Now.AddDays(30);
+            DateTimeOffset DateCookieExpires = new DateTimeOffset(DateTime.SpecifyKind(cookieExpireDate, DateTimeKind.Unspecified));
+
+
+            // create the jwt string
+            JsonWebToken jsonWebToken = new JsonWebToken(appSettings.jwtsecretKey);
+            string jwt = jsonWebToken.createAdminJWT(DateCookieExpires.DateTime);
+
+
+            // create the cookie
+            Cookies cookies = new Cookies(appSettings.DomainName);
+            cookies.CreateCookie(httpResponse, Cookies.AdminCookieName, jwt, DateCookieExpires);
+
+            return jwt;
+        }
+
+
+        /// <summary>
+        /// Creates response message to let the client know they were not logged in.
+        /// </summary>
+        /// <returns></returns>
+        private LoginAuthentication CreateBadLogin()
+        {
+            LoginAuthentication loginAuthentication = new LoginAuthentication();
+            loginAuthentication._errorMessage = "Invalid Login";
+            loginAuthentication._isLoggedIn = false;
+
+            return loginAuthentication;
+        }
+
+    }
+}
