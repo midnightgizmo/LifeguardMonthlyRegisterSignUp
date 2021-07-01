2e277e6ea54710b39929969c1efba63938076703
midnightgizmo <scottamillard@hotmail.com>
2021-07-01 14:58:54 +0100

    feat(admin): create route for GetAllActiveUsers

---
+++ Server/AspNetCore/MonthlyLifeguardRegister/Controllers/Admin/Users/UsersAdminController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using MonthlyLifeguardRegister.Attributes;
+using MonthlyLifeguardRegister.ControllersLogic.Admin.Users;
+using MonthlyLifeguardRegister.Models;
+using MonthlyLifeguardRegister.Models.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonthlyLifeguardRegister.Controllers.Admin.Users
+{
+    [ApiController]
+    [Route("API/admin/Users")]
+    public class UsersAdminController : ControllerBase
+    {
+
+        public UsersAdminController(IOptions<AppSettings> appSettings)
+        {
+            this.AppSettings = appSettings.Value;
+        }
+
+        /// <summary>
+        /// The settings from the appsettings.json file
+        /// </summary>
+        public AppSettings AppSettings { get; }
+
+
+        /// <summary>
+        /// Gets a list of all active users in the database and returns them as json
+        /// </summary>
+        /// <returns></returns>
+        [Authorize(AuthorizationType = AuthorizeType.Admin)]
+        [HttpGet]
+        [Route("getAllActiveUsers.php")]
+        [Produces("application/json")]
+        public List<UserFullDetails> GetAllActiveUsers()
+        {
+            AdminGetAllActiveUsersControllerLogic adminGetAllActiveUsersControllerLogic;
+
+            adminGetAllActiveUsersControllerLogic = new AdminGetAllActiveUsersControllerLogic();
+
+            return adminGetAllActiveUsersControllerLogic.GetAllActiveUsers_CreateResponse(this.AppSettings);
+        }
+    }
+}
---
+++ Server/AspNetCore/MonthlyLifeguardRegister/ControllersLogic/Admin/Users/AdminGetAllActiveUsersControllerLogic.cs
@@ -0,0 +1,64 @@
+﻿using MonthlyLifeguardRegister.Classess.Database;
+using MonthlyLifeguardRegister.Classess.Database.User;
+using MonthlyLifeguardRegister.Models;
+using MonthlyLifeguardRegister.Models.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonthlyLifeguardRegister.ControllersLogic.Admin.Users
+{
+    public class AdminGetAllActiveUsersControllerLogic
+    {
+        /// <summary>
+        /// Gets all users from the database who have been set as active
+        /// </summary>
+        /// <param name="appSettings">Used to get the database location</param>
+        /// <returns>List of active users</returns>
+        public List<UserFullDetails> GetAllActiveUsers_CreateResponse(AppSettings appSettings)
+        {
+            return this.GetAllActiveUsers(appSettings.sqlConectionStringLocation);
+        }
+
+        /// <summary>
+        /// Gets all active users from the database
+        /// </summary>
+        /// <param name="ConnectionString">physical location of database on disk</param>
+        /// <returns></returns>
+        public List<UserFullDetails> GetAllActiveUsers(string ConnectionStringLocation)
+        {
+            SqLiteConnection sqlCon;
+            dbSQLiteUser dbUser;
+            List<UserFullDetails> userFullDetailsList;
+
+            sqlCon = new SqLiteConnection();
+            sqlCon.OpenConnection(ConnectionStringLocation);
+
+            dbUser = new dbSQLiteUser(sqlCon);
+            userFullDetailsList = dbUser.SelectAllActiveUsers();
+
+            sqlCon.CloseConnection();
+
+            // goes through each users and sets the password property to string.Empty
+            this.RemovePassword(userFullDetailsList);
+
+            return userFullDetailsList;
+
+        }
+
+
+        /// <summary>
+        /// Removes the value from the password property of each user
+        /// </summary>
+        /// <param name="userFullDetailsList"></param>
+        private void RemovePassword(List<UserFullDetails> userFullDetailsList)
+        {
+            // go through each user and set the password property to string.Empty
+            foreach (UserFullDetails aUser in userFullDetailsList)
+                aUser.password = string.Empty;
+        }
+
+        
+    }
+}
